e5e94101d82e4a977a06bca4b9bc2b5f562fe9ec
sn0w12 <lucasskold49@gmail.com>
2024-01-04 12:25:10 +0100

    Updated requirements

--- run.bat
+++ run.bat
@@ -1,4 +1,8 @@
 @echo off
 CALL "C:\ProgramData\anaconda3\Scripts\activate.bat"
+IF %ERRORLEVEL% EQU 1 (
+    echo Installing missing packages...
+    pip install -r requirements.txt
+)
 python ai.py
 pause
