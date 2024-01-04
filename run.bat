a2f4530fbd1fb597d189ba70df0fb352c71fd890
sn0w12 <lucasskold49@gmail.com>
2024-01-04 12:30:13 +0100

    run.bat now automatically downloads requirements

--- run.bat
+++ run.bat
@@ -1,8 +1,10 @@
 @echo off
 CALL "C:\ProgramData\anaconda3\Scripts\activate.bat"
+python ai.py
 IF %ERRORLEVEL% EQU 1 (
     echo Installing missing packages...
     pip install -r requirements.txt
+	cls
+	python ai.py
 )
-python ai.py
 pause
