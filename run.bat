ca46314056cebdb711dbc6bab94c019deed947c2
sn0w12 <lucasskold49@gmail.com>
2024-01-06 20:10:37 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,12 +7,13 @@ python check_dependencies.py > missing_packages.txt
 SET DEPENDENCY_CHECK=%ERRORLEVEL%
 IF !DEPENDENCY_CHECK! EQU 1 (
     echo Missing packages found.
-    set /p UserResponse=Do you want to install missing packages? [Y/N]: 
+    set /p UserResponse=Do you want to install missing packages? [Y/n]: 
     IF /I "!UserResponse!"=="Y" (
         echo Installing missing packages...
         for /F "tokens=*" %%i in (missing_packages.txt) do pip install %%i
     ) ELSE (
         echo Skipping package installation.
+        exit
     )
 )
 
@@ -22,7 +23,7 @@ python ai.py
 
 :set_exit_option
 echo.
-set /p UserExit=Do you want to exit? [Y/N]: 
+set /p UserExit=Do you want to exit? [Y/n]: 
 IF /I "!UserExit!"=="Y" (
     goto end_script
 ) ELSE (
